59fc6e9c3766f96a867cfd7e316a160be79a2287
lukaslinares <lukaslinares12@gmail.com>
2022-11-17 17:36:04 -0800

    Populating comments

--- controllers/places.js
+++ controllers/places.js
@@ -37,14 +37,16 @@ router.get('/new', (req, res) => {
 
 //SHOW +
 router.get('/:id', (req, res) => {
-    Places.findById(req.params.id)
-    .then(place => {
-        res.render('places/show', { place })
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
+  Places.findById(req.params.id)
+  .populate('comments')
+  .then(place => {
+      console.log(place.comments)
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
 //UPDATE
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,7 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-  // console.log('data:', data)
+  let comments = (
+    <h3 className='inactive'>
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
         <Def>
           <main>
@@ -45,7 +63,7 @@ function show (data) {
             <hr/>
             <footer>
               <h2>Comments</h2>
-              <p>No comments yet!</p>
+              {comments}
               <br/>
             </footer>
           </main>

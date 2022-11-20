05b47607d12db7ce7f79b22d2cf2e4760bb19d29
lukaslinares <lukaslinares12@gmail.com>
2022-11-19 17:03:40 -0800

    Calculating place ratings average

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,11 +7,25 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className='inactive'>
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating = (
+      <h3>
+      {Math.round(averageRating)} stars
+      </h3>
+    )    
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
           <h4>{c.content}</h4>
           <h3>
             <stong>- {c.author}</stong>
@@ -33,6 +47,7 @@ function show (data) {
                 <div class="col-sm-6 mt-3">
                   <h1>{data.place.name}</h1>
                   <h2>Rating</h2>
+                  {rating}
                   <p>Not Rated</p>
                   <h2>Description</h2>
                   <h3>
@@ -66,6 +81,33 @@ function show (data) {
               {comments}
               <br/>
             </footer>
+            <hr />
+            <h2>Got Your Own Rant or Rave?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                  <label class="form-check-label" for="flexCheckDefault">
+                    Rant?
+                  </label>
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>
           </main>
         </Def>
     )
